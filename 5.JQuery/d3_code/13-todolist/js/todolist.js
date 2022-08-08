d06db61a1b1b9d12e2bd1e1a943a6e49531d10fb
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-08-08 23:18:04 +0800

    Update todolist.js
    更新

--- 5.JQuery/d3_code/13-todolist/js/todolist.js
+++ 5.JQuery/d3_code/13-todolist/js/todolist.js
@@ -9,19 +9,62 @@ $(
         // 回车 keycode 13
         $('#title').on('keydown', function (event) {
             if (event.keyCode === 13) {
-                // alert('111');
-                //先读取本地存储原来的数据
-                var local = getData();
-                // console.log(local);
-                // 把local数组进行更新 把数据追加给local数组
-                local.push({ title: $(this).val(), done: false });
-                // local存储到本地存储
-                saveData(local);
-                // 加载
-                load();
+                if ($(this).val() !== "") {
+                    // alert('111');
+                    //先读取本地存储原来的数据
+                    var local = getData();
+                    // console.log(local);
+                    // 把local数组进行更新 把数据追加给local数组
+                    local.push({ title: $(this).val(), done: false });
+                    // local存储到本地存储
+                    saveData(local);
+                    // 加载
+                    load();
+
+                    $(this).val("");
+                } else {
+                    alert("请输入内容");
+                }
 
             }
         });
+
+        // 3.删除操作
+        $("ol").on("click", "a", function () {
+            // alert("111");
+            // 先获取本地存储
+            var data = getData();
+            console.log(data);
+
+            // 修改数据
+            var index = $(this).attr("id");
+            console.log(index);
+
+            // 删除数据
+            // 从index位置开始 删除1个  
+            data.splice(index, 1);
+
+            // 保存数组
+            saveData(data);
+
+            // 渲染
+            load();
+        });
+
+        // 4、切换ol和ul的内容
+        $("ol,ul").on("click", "input", function () {
+            // alert(111);
+            // 获取本地数据
+            var data = getData();
+            // 修改本地数据
+            var index = $(this).siblings("a").attr("id");
+            data[index].done = $(this).prop("checked");
+
+            // 保存本地数据
+            saveData(data);
+            // 渲染本地数据
+            load();
+        })
 
 
 
@@ -48,13 +91,29 @@ $(
             var data = getData();
             // console.log(data);
             // 遍历之前清空元素内容
-            $("ol").empty();
+            $("ol,ul").empty();
             // 遍历数据
+
+            var todocount = 0;
+            var donecount = 0;
+
             $.each(data, function (i, n) {
                 // console.log(i);
                 // console.log(n);
-                $('ol').prepend("<li> <input type = 'checkbox'> <p>" + n.title + "</p> <a href = 'javascript:;'></a> </li>");
-            })
+                if (n.done) {
+                    $('ul').prepend("<li> <input type = 'checkbox' checked = 'checked'> <p>" + n.title + "</p> <a href = 'javascript:;' id = " + i + "></a> </li>");
+                    donecount++;
+                } else {
+                    $('ol').prepend("<li> <input type = 'checkbox'> <p>" + n.title + "</p> <a href = 'javascript:;' id = " + i + "></a> </li>");
+                    todocount++;
+                }
+
+            });
+
+            $("#todocount").text(todocount);
+            $("#donecount").text(donecount);
+
+
         }
 
     }
