a2c81af5a077db45f31fb4e977fc01d44a43c444
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-08-25 17:21:29 +0800

    vue

--- 9.Vue.js快速入门/04-源代码/demo-悦听player/js/main.js
+++ 9.Vue.js快速入门/04-源代码/demo-悦听player/js/main.js
@@ -48,76 +48,76 @@ var app = new Vue({
   },
   methods: {
     // 歌曲搜索
-    searchMusic: function() {
+    searchMusic: function () {
       var that = this;
       axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
-        function(response) {
+        function (response) {
           // console.log(response);
           that.musicList = response.data.result.songs;
           console.log(response.data.result.songs);
         },
-        function(err) {}
+        function (err) { console.log(err); }
       );
     },
     // 歌曲播放
-    playMusic: function(musicId) {
+    playMusic: function (musicId) {
       //   console.log(musicId);
       var that = this;
       // 获取歌曲地址
       axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
-        function(response) {
+        function (response) {
           // console.log(response);
           // console.log(response.data.data[0].url);
           that.musicUrl = response.data.data[0].url;
         },
-        function(err) {}
+        function (err) { }
       );
 
       // 歌曲详情获取
       axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
-        function(response) {
+        function (response) {
           // console.log(response);
           // console.log(response.data.songs[0].al.picUrl);
           that.musicCover = response.data.songs[0].al.picUrl;
         },
-        function(err) {}
+        function (err) { console.log(err); }
       );
 
       // 歌曲评论获取
       axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
-        function(response) {
+        function (response) {
           // console.log(response);
           // console.log(response.data.hotComments);
           that.hotComments = response.data.hotComments;
         },
-        function(err) {}
+        function (err) { }
       );
     },
     // 歌曲播放
-    play: function() {
+    play: function () {
       // console.log("play");
       this.isPlaying = true;
     },
     // 歌曲暂停
-    pause: function() {
+    pause: function () {
       // console.log("pause");
       this.isPlaying = false;
     },
     // 播放mv
-    playMV: function(mvid) {
+    playMV: function (mvid) {
       var that = this;
       axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
-        function(response) {
+        function (response) {
           // console.log(response);
           console.log(response.data.data.url);
           that.isShow = true;
           that.mvUrl = response.data.data.url;
         },
-        function(err) {}
+        function (err) { }
       );
     },
     // 隐藏
-    hide: function() {
+    hide: function () {
       this.isShow = false;
     }
   }
