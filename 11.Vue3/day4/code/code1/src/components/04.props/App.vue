63c3ac71e2bace0b23c07dbfca5989f02d4f747d
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-09-13 15:46:52 +0800

    vue3

---
+++ 11.Vue3/day4/code/code1/src/components/04.props/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>这是 04 App.vue 根组件</h1>
+    <hr />
+    <!-- v-bind动态绑定 -->
+    <my-article :title="info.title" :author="'post by ' + info.author" pub-time="1989"></my-article>
+  </div>
+</template>
+
+<script>
+import MyArticle from './Article.vue'
+
+export default {
+  name: 'MyApp',
+  data() {
+    return {
+      info: {
+        title: 'abc',
+        author: '123',
+      },
+    }
+  },
+  components: {
+    MyArticle,
+  },
+}
+</script>
