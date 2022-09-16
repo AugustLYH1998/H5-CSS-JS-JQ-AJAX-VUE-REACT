7c287bc9247ace3d60fda87fb3a3c89ae9380f95
AugustLYH1998 <60884610+AugustLYH1998@users.noreply.github.com>
2022-09-16 11:17:46 +0800

    vue3

---
+++ 11.Vue3/day5/code/code1/src/components/01.Count/Count.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>数量：{{ count }}</p>
+    <p>状态：{{ state }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCount',
+  // props: ['count', 'state'],
+  props: {
+    count: {
+      // 声明必填项
+      type: Number,
+      required: true,
+      default: 100
+    },
+    state: Boolean,
+
+    // 多个可能类型
+    info: [String, Number],
+    type: {
+      // 自定义验证函数validator
+      validator(value) {
+        // -1 验证 失败，否则成功
+        return ['success', 'warning', 'danger'].indexOf(value) !== -1
+      }
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped></style>
